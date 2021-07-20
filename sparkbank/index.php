9383af79e18433bd29c4711eee4896f0fdc74199
Nikita Kulloli <85385319+NikitaKulloli@users.noreply.github.com>
2021-07-20 12:36:19 +0530

    Add files via upload

---
+++ sparkbank/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>spark</title>
+</head>
+
+<body>
+<div class="bg">
+<?php
+include 'header.php';
+?>
+<div class="section">
+        <h1>welcome to Spark bank </h1>
+</div>
+        <footer>
+            <p>All rights are reserved &copy 2020. <a href='#'>spark bank</a></p>
+        </footer>
+    </div>
+</body>
+
+</html>
